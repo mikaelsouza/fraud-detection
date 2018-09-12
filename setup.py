8885eabd7a612869817435e1e24492beaa1faf7d
Mikael Souza <mss2@icomp.ufam.edu.br>
2018-09-12 17:50:24 -0400

    Added stratified crossvalidation to lib

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fraudutils',
-      version='0.0.2',
+      version='0.0.3',
       description='A simple lib with util functions',
       url='',
       author='Mikael Souza',
